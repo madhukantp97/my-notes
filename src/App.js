db9fe20f886cc770000a7c5796621764eb0fc679
madhukant <madhukantp97@gmail.com>
2024-11-07 18:48:42 +0530

    removed login and register

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 // src/App.js
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import StickyNotes from './StickyNotes';
 import Calculator from './Calculator';
 import ToDoList from './ToDoList';
-import Login from './Login';
-import Register from './Register'; // Import the Register page
+// import Login from './Login';
+// import Register from './Register'; // Import the Register page
 import './App.css';
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isRegistered, setIsRegistered] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isRegistered, setIsRegistered] = useState(false);
 
-  useEffect(() => {
-    const registeredStatus = localStorage.getItem('isRegistered');
-    const loggedInStatus = localStorage.getItem('isLoggedIn');
+  // useEffect(() => {
+  //   const registeredStatus = localStorage.getItem('isRegistered');
+  //   const loggedInStatus = localStorage.getItem('isLoggedIn');
 
-    if (registeredStatus === 'true') {
-      setIsRegistered(true);
-    }
+  //   if (registeredStatus === 'true') {
+  //     setIsRegistered(true);
+  //   }
 
-    if (loggedInStatus === 'true') {
-      setIsLoggedIn(true);
-    }
-  }, []);
+  //   if (loggedInStatus === 'true') {
+  //     setIsLoggedIn(true);
+  //   }
+  // }, []);
 
   return (
     <Router>
@@ -33,21 +33,27 @@ const App = () => {
         <Link to="/calculator">Calculator</Link>
       </nav>
       <Routes>
+        <Route path="/" element={<StickyNotes />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/todo" element={<ToDoList />} /> {/* Route for To-Do List */}
+      </Routes>
+
+      {/* <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={isRegistered ? <Navigate to="/login" /> : <Register />} /> {/* If registered, redirect to login */}
-        <Route 
-          path="/" 
-          element={isLoggedIn ? <StickyNotes /> : <Navigate to="/login" />} 
-        />
-        <Route 
-          path="/calculator" 
-          element={isLoggedIn ? <Calculator /> : <Navigate to="/login" />} 
-        />
-        <Route 
-          path="/todo" 
-          element={isLoggedIn ? <ToDoList /> : <Navigate to="/login" />} 
-        />
-      </Routes>
+        {/* <Route  */}
+          {/* path="/"  */}
+          {/* element={isLoggedIn ? <StickyNotes /> : <Navigate to="/login" />}  */}
+        {/* /> */}
+        {/* <Route  */}
+          {/* path="/calculator"  */}
+          {/* element={isLoggedIn ? <Calculator /> : <Navigate to="/login" />}  */}
+        {/* /> */}
+        {/* <Route  */}
+          {/* path="/todo"  */}
+          {/* element={isLoggedIn ? <ToDoList /> : <Navigate to="/login" />}  */}
+        {/* /> */}
+      {/* </Routes> */}
     </Router>
   );
 };
