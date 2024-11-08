f82ebe4c382a079fce8683216345c6cf85045d8d
madhukant <madhukantp97@gmail.com>
2024-11-08 12:50:55 +0530

    deleted login and registration page

--- src/App.js
+++ src/App.js
@@ -4,26 +4,9 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import StickyNotes from './StickyNotes';
 import Calculator from './Calculator';
 import ToDoList from './ToDoList';
-// import Login from './Login';
-// import Register from './Register'; // Import the Register page
 import './App.css';
 
 const App = () => {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const [isRegistered, setIsRegistered] = useState(false);
-
-  // useEffect(() => {
-  //   const registeredStatus = localStorage.getItem('isRegistered');
-  //   const loggedInStatus = localStorage.getItem('isLoggedIn');
-
-  //   if (registeredStatus === 'true') {
-  //     setIsRegistered(true);
-  //   }
-
-  //   if (loggedInStatus === 'true') {
-  //     setIsLoggedIn(true);
-  //   }
-  // }, []);
 
   return (
     <Router>
@@ -38,22 +21,6 @@ const App = () => {
         <Route path="/todo" element={<ToDoList />} /> {/* Route for To-Do List */}
       </Routes>
 
-      {/* <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={isRegistered ? <Navigate to="/login" /> : <Register />} /> {/* If registered, redirect to login */}
-        {/* <Route  */}
-          {/* path="/"  */}
-          {/* element={isLoggedIn ? <StickyNotes /> : <Navigate to="/login" />}  */}
-        {/* /> */}
-        {/* <Route  */}
-          {/* path="/calculator"  */}
-          {/* element={isLoggedIn ? <Calculator /> : <Navigate to="/login" />}  */}
-        {/* /> */}
-        {/* <Route  */}
-          {/* path="/todo"  */}
-          {/* element={isLoggedIn ? <ToDoList /> : <Navigate to="/login" />}  */}
-        {/* /> */}
-      {/* </Routes> */}
     </Router>
   );
 };
