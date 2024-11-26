e1a0433aeb71870161cdf7c188448801543b1ad9
madhukant <madhukantp97@gmail.com>
2024-11-26 11:57:23 +0530

    removed unit conversion

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import StickyNotes from './StickyNotes';
 import Calculator from './Calculator';
 import ToDoList from './ToDoList';
 import CalendarReminder from './CalendarReminder';
-import UnitConversion from './UnitConversion'; // import the new component
 import './App.css';
 
 const App = () => {
@@ -16,14 +15,12 @@ const App = () => {
         <Link to="/todo">To-Do List</Link>
         <Link to="/calculator">Calculator</Link>
         <Link to="/calendar">Calendar</Link>
-        <Link to="/unit-conversion">Unit Conversion</Link> {/* Add link to Unit Conversion */}
       </nav>
       <Routes>
         <Route path="/" element={<StickyNotes />} />
         <Route path="/calculator" element={<Calculator />} />
         <Route path="/todo" element={<ToDoList />} />
         <Route path="/calendar" element={<CalendarReminder />} />
-        <Route path="/unit-conversion" element={<UnitConversion />} /> {/* Add route for Unit Conversion */}
       </Routes>
     </Router>
   );
